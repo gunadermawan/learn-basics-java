98070272cfdf32aeaa8c25695ad6629890784651
gunadermawan <gunadermawan.official@gmail.com>
2022-12-20 17:00:33 +0700

    operator in java

---
+++ src/com/gunder/javafundamental/operator/OperatorAritmatika.java
@@ -0,0 +1,28 @@
+package com.gunder.javafundamental.operator;
+
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        System.out.println("Operasi Penjumlahan");
+        int hasilPenjumlahan = 5 + 1;
+        System.out.println("Hasil 5 + 1 = " + hasilPenjumlahan);
+        System.out.println();
+        System.out.println("Operasi Pengurangan");
+        int hasilPengurangan = 4 - 1;
+        System.out.println("Hasil 4 - 1 = " + hasilPengurangan);
+        System.out.println();
+        System.out.println("Operasi Pengalian");
+        int hasilPengalian = 5 * 5;
+        System.out.println("Hasil 5 * 5 = " + hasilPengalian);
+        System.out.println();
+        System.out.println("Operasi Pembagian");
+        int hasilPembagian = 20 / 2;
+        System.out.println("Hasil 20 / 2 = " + hasilPembagian);
+        System.out.println();
+        System.out.println("Operasi Habis bagi");
+        int hasilSisa = 8 % 2;
+        System.out.println("Hasil 8 % 2 = " + hasilSisa);
+        int hasilSisaLain = 9 % 2;
+        System.out.println("Hasil 9 % 2 = " + hasilSisaLain);
+        System.out.println();
+    }
+}
