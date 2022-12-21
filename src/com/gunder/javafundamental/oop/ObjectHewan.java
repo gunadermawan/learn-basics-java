d4bc4e5b9e098f16912b5fe6641ac108c82b4291
gunadermawan <gunadermawan.official@gmail.com>
2022-12-21 14:17:18 +0700

    oop class and object in java

---
+++ src/com/gunder/javafundamental/oop/ObjectHewan.java
@@ -0,0 +1,11 @@
+package com.gunder.javafundamental.oop;
+
+public class ObjectHewan {
+    public void cetakNama(String name) {
+        System.out.println("nama yang tercetak: " + name);
+    }
+
+    public void beratBadan(int berat) {
+        System.out.println("berat badan: " + berat);
+    }
+}
