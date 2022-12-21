e6c6918712e3b3622acd604903f8b8e0b9255f39
gunadermawan <gunadermawan.official@gmail.com>
2022-12-21 10:39:16 +0700

    loopinh java

---
+++ src/com/gunder/javafundamental/looping/While.java
@@ -0,0 +1,12 @@
+package com.gunder.javafundamental.looping;
+
+public class While {
+    public static void main(String[] args) {
+        int value = 5;
+        while (value <= 10) {
+            System.out.println("Angka " + value);
+            value++;
+            System.out.println("\n");
+        }
+    }
+}
