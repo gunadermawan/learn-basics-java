ddec6265b2d4b6b8d74fa0fc504b475de8c1d3cf
gunadermawan <gunadermawan.official@gmail.com>
2022-12-24 11:48:50 +0700

    inheritance, overloading dan overriding pada java

--- src/com/gunder/javafundamental/oop/inheritance/Kucing.java
+++ src/com/gunder/javafundamental/oop/inheritance/Kucing.java
@@ -1,8 +1,42 @@
 package com.gunder.javafundamental.oop.inheritance;
 
 public class Kucing extends Hewan {
+
+    private String ras;
+    private String habitat;
+
+    public Kucing(String ras, String habitat) {
+        this.ras = ras;
+        this.habitat = habitat;
+    }
+
+    @Override
+    public String toString() {
+        return "kucing ras: " + ras + ", habitat: " + habitat;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Kucing) {
+            Kucing other = (Kucing) obj;
+            return this.ras.equals(other.ras);
+        } else {
+            return false;
+        }
+    }
+
     public Kucing() {
         super();
         System.out.println("constructor class kucing");
+    }
+
+    @Override
+    public void makan() {
+        super.makan();
+        System.out.println("Kucing sedang makan..");
+    }
+
+    public void makan(String food) {
+        System.out.println("kucing makan " + food);
     }
 }
--- src/com/gunder/javafundamental/oop/inheritance/Main.java
+++ src/com/gunder/javafundamental/oop/inheritance/Main.java
@@ -15,5 +15,21 @@ public class Main {
         System.out.println("apakah hewan IS-A objek -> " + (kucing instanceof Object));
         System.out.println("apakah kucing IS-A hewan -> " + (kucing instanceof Hewan));
         System.out.println("apakah kucing IS-A kucing -> " + (kucing instanceof Kucing));
+        hewan.makan();
+        kucing.makan();
+        kucing.makan("tempe");
+        for (int i = 1; i <= 20; i++) {
+            System.out.print("-");
+        }
+        Kucing meow = new Kucing("Ocicat", "tropis");
+        Kucing puss = new Kucing("Ocicat", "subtropis");
+        Kucing popo = new Kucing("Anggora", "subtropis");
+
+        System.out.println(meow.toString());
+        System.out.println(puss.toString());
+        System.out.println(popo.toString());
+
+        System.out.println("meow equals puss? " + meow.equals(puss));
+        System.out.println("meow equals popo? " + meow.equals(popo));
     }
 }
